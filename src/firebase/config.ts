67b17f0cec850e14b2675ba9377aa4fe0e107452
hustler hustler <hustlerh3848@gmail.com>
2025-11-16 15:17:31 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3257811269-808de",
+  "appId": "1:912556752646:web:c50bdbf8932435806d2887",
+  "apiKey": "AIzaSyADOX-yVJWxtOT1vO9HRov3YFo8smIMsz8",
+  "authDomain": "studio-3257811269-808de.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "912556752646"
+};
