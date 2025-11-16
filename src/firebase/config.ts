87cf8db2c7244f1ecb1a2c145ce179de25b7aa4d
hustler hustler <hustlerh3848@gmail.com>
2025-11-16 16:19:10 +0000

    Automatic initialization failed. Falling back to firebase config object.

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,3 +1,5 @@
+import 'dotenv/config';
+
 export const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
@@ -5,5 +7,5 @@ export const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
